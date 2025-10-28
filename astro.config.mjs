0547841f6e2ee00f8827178a97e46d8469610397
D4VGFX <d4v.gfx@gmail.com>
2025-10-28 11:32:01 +0100

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://alexnatola.it',
-  base: '/',
+  site: 'https://alexnatola.com',
   output: 'static',
+  outDir: 'docs',
 });
